c0ccf6d0606a41521cd60158a0b6cda1596546ea
Skyestuck <jdhumphrey2001@hotmail.com>
2026-03-11 16:36:22 -0400

    Working on player swap to living zombie after death
    figuring out best method for this

---
+++ zombiegame/Scenes/trap.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+var has_damaged := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	if get_tree().get_nodes_in_group("player").is_empty():
+		print("Group is Empty!")
+	else:
+		print_group("player")
+		print("^ Group contains ^")
+
+func print_group(group_name: String) -> void:
+	var members = get_tree().get_nodes_in_group(group_name)
+	for node in members:
+		print(node)  # prints the node reference
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	# Only damage if we haven't already damaged during this entry
+	if not has_damaged and body.is_in_group("player"):
+		body.take_damage(1)   # Calls the player’s damage method
+		has_damaged = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		has_damaged = false
--- zombiegame/Scripts/ZombieCharacter.gd
+++ zombiegame/Scripts/ZombieCharacter.gd
@@ -1,14 +1,54 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 30.0
+@export var SPEED = 60.0
+@export var playerhp: int = 1
+@export var lifetime: int = 0
 
+signal died
+
+func _ready():
+	add_to_group("all_players")
+	if get_tree().get_nodes_in_group("player").is_empty():
+		add_to_group("player")
+		print("I'm player")
+	else:
+		print("Player exists")
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input := Input.get_vector("MoveLeft","MoveRight","MoveUp","MoveDown")
-	
-	velocity = input * SPEED
-	
-	move_and_slide()
+	if is_in_group("player"):
+		var input := Input.get_vector("MoveLeft","MoveRight","MoveUp","MoveDown")
+		lifetime += 1
+		velocity = input * SPEED
+		
+		move_and_slide()
+	else:
+		pass
+
+func take_damage(amount: int) -> void:
+	playerhp -= amount
+	print("HP: ", playerhp)
+	if playerhp <= 0:
+		print("A Zombie Died!")
+		remove_from_group("player")
+		queue_free()
+		get_tree().call_group("all players", "_on_any_player_died", self)
+
+
+func on_player_died() -> void:
+	print("Player Died!")
+	if is_queued_for_deletion(): return  # this is the dying node; skip
+	if get_tree().get_nodes_in_group("player").is_empty():
+		add_to_group("player")
+		print("I'm the player now!")
+	else:
+		print("I'm Still Detecting A Player!")
+		
+func _on_any_player_died(dead: Node) -> void:
+	if self == dead: return
+	if !is_inside_tree(): return
+	if get_tree().get_nodes_in_group("player").is_empty():
+		add_to_group("player")
+		print ("I'm the player now: ", name)
