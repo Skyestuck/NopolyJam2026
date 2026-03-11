7845a4a31ac1d644a1ce4da53d6f5bb3d2df0106
Skyestuck <jdhumphrey2001@hotmail.com>
2026-03-11 11:09:28 -0400

    Changed Vector Input
    Changed the Input Vector code to use Godot's built-in one. Normalizes movement automatically. Way less lines.

--- zombiegame/Scripts/ZombieCharacter.gd
+++ zombiegame/Scripts/ZombieCharacter.gd
@@ -1,18 +1,14 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 300.0
+@export var SPEED = 30.0
 
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x := Input.get_action_strength("MoveRight") - Input.get_action_strength("MoveLeft")
-	var y := Input.get_action_strength("MoveDown") - Input.get_action_strength("MoveUp")
-	var input := Vector2(x,y)
+	var input := Input.get_vector("MoveLeft","MoveRight","MoveUp","MoveDown")
 	
 	velocity = input * SPEED
-	
-	print("x=", x, " y=", y, " vel=", velocity)
 	
 	move_and_slide()
